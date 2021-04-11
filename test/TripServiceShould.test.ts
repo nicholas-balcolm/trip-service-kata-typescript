8bca2c2829aa11075661733c94fb54d33a19ea18
nicholas-balcolm <nicholas.balcolm@gmail.com>
2021-04-11 20:52:33 +0000

    test: first expected behavior

--- test/TripServiceShould.test.ts
+++ test/TripServiceShould.test.ts
@@ -1,7 +1,18 @@
 import "jest";
+import UserNotLoggedInException from "../src/exception/UserNotLoggedInException";
+import TripService from "../src/trip/TripService";
 
 describe("The TripService", () => {
+    let service: TripService;
+    beforeEach(() => {
+        service = new TripService;
+    })
+
     it("adds 2 and 2", () => {
         expect(2 + 2).toBe(5);
+    });
+
+    it("given null user, throws UserNotLoggedInException", ()=> {
+       expect(() => service.getTripsByUser(null)).toThrow(UserNotLoggedInException)
     });
 });
